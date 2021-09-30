a234d51562dc83ddbae8d698cd081671f874f14b
ojo-perezoso <bymartin24@gmail.com>
2021-09-30 10:48:03 -0700

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	/*This is the main function
+	 * it is an example of the puts function
+	*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);       
+}
