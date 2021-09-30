3b7feec8fd710123eafd94a3428b9db54918010e
ojo-perezoso <bymartin24@gmail.com>
2021-09-30 11:10:20 -0700

    Added 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main function for exercice 5
+ *
+ * Return: the number 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
