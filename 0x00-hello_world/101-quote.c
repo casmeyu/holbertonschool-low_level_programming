2a2804b25c39c0b2fd9479a4594284c43e4f5484
ojo-perezoso <bymartin24@gmail.com>
2021-09-30 12:35:25 -0700

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -This is the main function for printinf without printf or puts
+ *
+ * Returns: the number 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
