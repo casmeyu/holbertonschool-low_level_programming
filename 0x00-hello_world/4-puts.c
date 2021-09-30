e54109c3c08dd6852cb03d570fdb41f4d613297f
ojo-perezoso <bymartin24@gmail.com>
2021-09-30 10:52:10 -0700

    fixed betty 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
-int main()
+int main(void)
 {
-	/*This is the main function
+	/*
+	 * This is the main function
 	 * it is an example of the puts function
 	*/
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);       
+	return (0);
 }
