ac4977d06376b410558b038700765544d0a55cf4
ojo-perezoso <bymartin24@gmail.com>
2021-10-01 07:06:06 -0700

    Added 1-last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - this is the main function for the last digit
+ *
+ * Return: the number 0
+ */
+int main(void)
+{
+	int n, count;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d is the second field", n % 10);
+	/* your code goes there */
+
+	return (0);
+}
