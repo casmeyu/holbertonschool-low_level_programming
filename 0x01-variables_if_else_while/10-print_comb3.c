4ab24a0c3bd6980ad7d07458c061b09eff7419e4
ojo-perezoso <bymartin24@gmail.com>
2021-10-01 12:50:32 -0700

    Added 10-print_comb3.c

---
+++ 0x01-variables_if_else_while/10-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - this is the main function for print_comb3
+ *
+ * Return: the number 0
+ *
+ */
+int main(void)
+{
+	int d1, d2;
+	
+	for (d1 = 48; d1 < 58; d1 ++)
+	{
+		for (d2 = 48; d2 < 58; d2++)
+		{
+			if (d2 > d1)
+			{
+				putchar(d1);
+                                putchar(d2);
+				if (d1 >= 56)
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+                                	putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
