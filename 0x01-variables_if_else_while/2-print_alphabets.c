79266cdfa6985f705c34daa0aa1cdb0ab4f05ae7
ojo-perezoso <bymartin24@gmail.com>
2021-10-01 10:02:24 -0700

    Added 2-print_alphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: the number 0
+ */
+int main(void)
+{
+	int c = 'A';
+
+	for (c; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
