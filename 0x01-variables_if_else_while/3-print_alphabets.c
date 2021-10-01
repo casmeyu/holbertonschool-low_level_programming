7f817dffa02c8aac6747748b6826705bb47d5b8c
ojo-perezoso <bymartin24@gmail.com>
2021-10-01 10:49:51 -0700

    Added 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is the main function for printing the alphabet
+ *
+ * Return: the number 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
