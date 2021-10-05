fdaf14c400ea7156e1baf8b5e074bbc89c7c5dfb
ojo-perezoso <bymartin24@gmail.com>
2021-10-05 09:36:52 -0700

    fixed comment of 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main - This funcion prints a lowercase alphabet and its a header file
+ * print_alphabet - This funcion prints a lowercase alphabet and its a header file
  *
- * Return: the number 0
  *
  */
 void print_alphabet(void)
