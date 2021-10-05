1f1db156b3a6cd764e8c5c60b333bd407b6ea711
ojo-perezoso <bymartin24@gmail.com>
2021-10-05 09:34:47 -0700

    fix try 2 1-alphabet changed function 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
-void print_alphabet(void);
 /**
  * main - This funcion prints a lowercase alphabet and its a header file
  *
  * Return: the number 0
  *
  */
-int main(void)
+void print_alphabet(void)
 {
 	int c;
 
@@ -15,5 +14,4 @@ int main(void)
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
