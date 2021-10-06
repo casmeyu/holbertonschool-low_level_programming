9fd023fd9774fa7673fa6438d2c3e9b068b80ac9
ojo-perezoso <bymartin24@gmail.com>
2021-10-06 11:51:37 -0700

    Added 0x03-debugging and 0-main.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,15 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
+		print_alphabet();
+		/*
 		int c;
-
+		
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 		_putchar('\n');
+		*/
 	}
 }
--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -1,4 +1,21 @@
+#include <stdio.h>
+
+int num1;
+void prueba(void)
+	{
+		int num1;
+		num1 = 3;
+		printf("%d", num1);
+		printf("location of num1 prueba: %pn\n", &num1);
+			
+	}
+
 int main(void)
 {
-	_putchar('c');
+	num1= 4;
+	printf("main %d", num1);
+	printf("location of num1 main: %pn\n", &num1);
+	prueba();
+	printf("main - %d\n", num1);
+
 }
