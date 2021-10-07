e7b201399bd8582adaaf15424f0a51d08879a135
ojo-perezoso <bymartin24@gmail.com>
2021-10-07 06:40:42 -0700

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers: this function prints most numbers -{2, 4}
+ *
+ */
+void print_most_numbers(void)
+{
+	int c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
