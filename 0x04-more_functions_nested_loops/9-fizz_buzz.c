9d3c61d27223df25eee67866942f8de0903c8dc3
ojo-perezoso <bymartin24@gmail.com>
2021-10-07 10:07:36 -0700

    Add 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - does the fizz buzz for the numbers from 0 to 100
+ *
+ * Return: the number 0
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Fuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+
+		if (n == 100)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(" ");
+		}
+	}
+
+	return (0);
+}
