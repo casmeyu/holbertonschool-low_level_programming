ea1f122591ea479418aca200584eb63012a20b7b
ojo-perezoso <bymartin24@gmail.com>
2021-10-06 19:18:31 -0700

    Added 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this function prints the first 50 numbers from fibonacci
+ *
+ * Return: the number 0
+ */
+int main(void)
+{
+	int cont, actual, anterior;
+	actual = 1;
+	anterior = 0;
+	for (cont = 1; cont <= 50; cont++)
+	{
+		actual = anterior + actual;
+		if (actual > 4000000)
+			break;
+		printf("%d, ", actual);
+		anterior = actual - anterior;
+	}
+	return (0);
+}
