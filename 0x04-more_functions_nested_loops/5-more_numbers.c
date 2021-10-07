6d249705892e8adb9456484bc2592876f9332b9c
ojo-perezoso <bymartin24@gmail.com>
2021-10-07 09:42:51 -0700

    fix try 1 5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,8 @@ void more_numbers(void)
 			if (n >= 10)
 			{
 				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
 			}
-			else
-			{
-				_putchar(n + '0');
-			}
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
