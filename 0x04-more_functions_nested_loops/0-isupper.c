dd7f43a18961f6fb9c7b30a0eb8391f406bf253b
ojo-perezoso <bymartin24@gmail.com>
2021-10-07 06:08:03 -0700

    Added exercice 2 and 3

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,9 @@
+/**
+ * _isupper: checks if entering c is upper case char
+ *
+ * @c: entering char
+ * Return: 1 or 0 (upper char / not upper char)
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - this function returns the multiplication
+ * of two integers
+ * @a: entering int
+ * @b: entering int
+ *
+ * Returns: int a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);	
+}
