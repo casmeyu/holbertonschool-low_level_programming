c21702b0361932673c76a2e07ca1a9243d6b01cb
ojo-perezoso <bymartin24@gmail.com>
2021-10-07 06:51:11 -0700

    Added 6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - this function takes a number and prints a line
+ *
+ * @n: number of "_" in line
+ */
+void print_line(int n)
+{
+	int c;
+	for (c = 0; c < n; c++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
