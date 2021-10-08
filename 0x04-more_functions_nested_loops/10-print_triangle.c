6e0c338006ecbbf6b25e32ff4eac294d3d6f9f18
ojo-perezoso <bymartin24@gmail.com>
2021-10-07 18:57:57 -0700

    coding style fix 1 10-print.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
 void print_triangle(int size)
 {
 	int lines, s, c;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -28,5 +28,5 @@ void print_triangle(int size)
 
 			_putchar('\n');
 		}
-	}	
+	}
 }
