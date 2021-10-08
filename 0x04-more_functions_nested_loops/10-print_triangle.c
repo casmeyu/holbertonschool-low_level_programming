9e4c0270f1d2a61ae21f0e576a676425247f1c0c
ojo-perezoso <bymartin24@gmail.com>
2021-10-07 18:56:42 -0700

    Added 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle based on the incoming in
+ *
+ * @size: int size of the triangle
+ */
+void print_triangle(int size)
+{
+	int lines, s, c;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (lines = 0; lines < size; lines++)
+		{
+			for (c = 0; c < (size - lines); c++)
+			{
+				_putchar(' ');
+			}
+			for (; c < size; c++)
+			{
+				_putchar('#');
+			}
+
+
+			_putchar('\n');
+		}
+	}	
+}
