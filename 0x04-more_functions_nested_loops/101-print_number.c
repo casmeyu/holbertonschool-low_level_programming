80542c340ef9f0b1e3cb60f3eecb6ead7e6a1ff9
ojo-perezoso <bymartin24@gmail.com>
2021-10-08 19:17:01 -0700

    added 101-print_number to main.h

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,18 +8,13 @@
 void print_number(int n)
 {
 	unsigned int pos = n;
-	/* 
-	 * If number is smaller than 0, put a - sign and convert n
-	 */
+
 	if (n < 0)
 	{
 		_putchar('-');
 		pos = -n;
 	}
 
-	/*
-	 * Divide by 10 and recur
-	 */
 	if (n / 10)
 		print_number(pos / 10);
 
