d9edcbcb9906f86756617e110acccda214edcaa6
ojo-perezoso <bymartin24@gmail.com>
2021-10-08 18:54:34 -0700

    Now i added 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - Takes an integer n long and prints its digits
+ * one by one recusively
+ *
+ * @n: number to be printed
+ */
+void print_number(int n)
+{
+	/* If number is smaller than 0, put a - sign and convert n */
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	/* Divide by 10 and recur*/
+	if (n / 10)
+	print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
+
