25b644edca94f165a2eb0969a9adf4451a1711fe
ojo-perezoso <bymartin24@gmail.com>
2021-10-12 11:22:25 -0700

    Added 4-print_rev.c with description

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - takes a string and prints it in reverse
+ * @s: the entering string
+ */
+void print_rev(char *s)
+{
+	int pos;
+
+	for (pos = 0; *(s + pos) != 0;)
+	{
+		pos++;
+	}
+	for (; pos >= 0; pos--)
+	{
+		_putchar(s[pos]);
+	}
+	_putchar('\n');
+}
