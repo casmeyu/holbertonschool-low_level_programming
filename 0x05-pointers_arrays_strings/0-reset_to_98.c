69172e49806f208207c8771bf2f6f96beaff3845
ojo-perezoso <bymartin24@gmail.com>
2021-10-12 09:21:57 -0700

    Added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - this functions resets the entering memory
+ * location value to 98
+ *
+ * @n: entering pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#0x05 pointers arrays and strings
+here are some basic aexerices about pointers, arrays and strings
