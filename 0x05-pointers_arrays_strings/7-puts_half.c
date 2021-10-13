6d12cee1353f502b35170b5e69241c5975df386f
ojo-perezoso <bymartin24@gmail.com>
2021-10-12 17:58:25 -0700

    Added 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prits the last half of a string
+ *
+ * @str: the entering string
+ */
+
+void puts_half(char *str)
+{
+	int len, pos;
+
+	for (len = 0; str[len] != 0;)
+		len++;
+
+	if (len % 2 == 0)
+		pos = len / 2;
+	else
+		pos = (len - 1) / 2;
+
+	while (str[pos] != 0)
+	{
+		_putchar(str[pos]);
+		pos++;
+	}
+
+}
