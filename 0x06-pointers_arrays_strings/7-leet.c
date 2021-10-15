b698bdc0ceb195817c84103f773d4e85181815aa
ojo-perezoso <bymartin24@gmail.com>
2021-10-15 15:11:39 -0700

    fix try 1 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * leet - this functions takes a strings and encodes it
  * according to leet
@@ -7,24 +8,23 @@
  *
  * Return: the processed string
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	int pos_s, pos_let;
+	int pos_str, pos_let;
 	char letter[5] = {'a', 'e', 'o', 't', 'l'};
 	int l_value[5] = {4, 3, 0, 7, 1};
 
-	for (pos_s = 0; s[pos_s] != 0; pos_s++)
+	for (pos_str = 0; str[pos_str] != 0; pos_str++)
 	{
 		for (pos_let = 0; letter[pos_let] != 0; pos_let++)
 		{
-			if (letter[pos_let] == s[pos_s] ||
-				(letter[pos_let] - 32) == s[pos_let])
+			if (letter[pos_let] == str[pos_str] || letter[pos_let] == str[pos_str] + 32)
 			{
-				s[pos_s] = (l_value[pos_let] + '0');
-				break;
+				str[pos_str] = (l_value[pos_let] + '0');
 			}
+
 		}
 	}
 
-	return (s);
+	return (str);
 }
