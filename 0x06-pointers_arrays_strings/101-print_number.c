cf5ca41dd398456fb84e8159360222c4835db392
ojo-perezoso <bymartin24@gmail.com>
2021-10-17 06:49:53 -0700

    Added 101-print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - takes an int as parameters and prints it only using _putchar
+ *
+ * @n: the entering int
+ */
+void print_number(int n)
+{
+	printf("print number of: %d", n);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+
+}
