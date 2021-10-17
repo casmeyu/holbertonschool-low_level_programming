bf885afc11915346c193b5056773f864643172a5
ojo-perezoso <bymartin24@gmail.com>
2021-10-17 07:13:17 -0700

    Changed variable holder to unsigned in 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,15 +6,16 @@
  */
 void print_number(int n)
 {
+	int unsigned pos = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		pos = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (pos / 10)
+		print_number(pos / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(pos % 10 + '0');
 
 }
