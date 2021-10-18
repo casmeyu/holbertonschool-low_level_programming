bbfe734b10cf7becb3d6ee0fe83f9fef8027adc4
ojo-perezoso <bymartin24@gmail.com>
2021-10-18 09:09:09 -0700

    first push

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strchr - Returns a pointer to the first occurrence of the character c in the 
- * string s, or NULL if the character is not found
- * 
+ * _strchr - Returns a pointer to the first occurrence of the character c
+ * in the string s, or NULL if the character is not found
+ *
  * @s: string to search in
  * @c: character to find
- * 
+ *
  * Return: the pointer to the character
  */
 char *_strchr(char *s, char c)
