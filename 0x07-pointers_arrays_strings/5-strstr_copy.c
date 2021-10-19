5dea3f34de177629be96e5cab71c8e2967b53b55
ojo-perezoso <bymartin24@gmail.com>
2021-10-18 19:10:28 -0700

    fix try 2 5-strstr.c... Thanks Berge...

--- 0x07-pointers_arrays_strings/5-strstr_copy.c
+++ 0x07-pointers_arrays_strings/5-strstr_copy.c
@@ -11,30 +11,31 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int pos_h = 0, pos_f, pos_x;
+	unsigned int pos_h, pos_f = 0;
 
-	if (needle[0] == '\0')
+	if (!needle)
 		return (haystack);
 
-	while (haystack[pos_h])
+	for (pos_h = 0; haystack[pos_h] != 0; pos_h++)
 	{
+
 		if (haystack[pos_h] == needle[0])
 		{
-			pos_x = pos_h;
-			pos_f = 0;
-
-			while (needle[pos_f] != '\0')
+			while (needle[pos_f] != 0)
 			{
-				if (haystack[pos_x] == needle[pos_f])
-					pos_x++, pos_f++;
+				if (haystack[pos_h + pos_f] == needle[pos_f])
+				{
+					pos_f++;
+				}
 				else
+				{
 					break;
+				}
 			}
 
-			if (needle[pos_f] == '\0' || haystack[pos_x] == '\0')
-				return (haystack + pos_h);
+			if (needle[pos_f] == 0 || haystack[pos_h + pos_f] == 0)
+				return (&haystack[pos_h]);
 		}
-		pos_h++;
 	}
-	return ('\0');
+	return (0);
 }
