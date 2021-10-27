4da39a762a4ed281383c2382e1615884773de1c3
ojo-perezoso <bymartin24@gmail.com>
2021-10-26 18:43:28 -0700

    final fix for right amount of memory allocated 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int argc, char *argv[])
 		for (pos_c = 0; argv[pos_a][pos_c] != 0; pos_c++)
 			c_len++;
 
-	con_args = malloc((c_len * sizeof(char) + argc));
+	con_args = malloc((c_len * sizeof(char) + argc + 1));
 
 	if (con_args == 0)
 		return (NULL);
