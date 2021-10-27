f90f7e0987d7677c08c5e7e5c591ec77aa471733
ojo-perezoso <bymartin24@gmail.com>
2021-10-26 18:42:15 -0700

    fix try 2, correct output on local machine

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int argc, char *argv[])
 		for (pos_c = 0; argv[pos_a][pos_c] != 0; pos_c++)
 			c_len++;
 
-	con_args = malloc(c_len * sizeof(char));
+	con_args = malloc((c_len * sizeof(char) + argc));
 
 	if (con_args == 0)
 		return (NULL);
