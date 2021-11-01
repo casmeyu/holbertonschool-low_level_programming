0d27189bb3971145a2a5ee0da577c370320bc72e
ojo-perezoso <bymartin24@gmail.com>
2021-11-01 10:05:18 -0700

    fix try 2 2-print_dog.c inverse the logic

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,18 +9,17 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name)
-			printf("Name: %s\n", d->name);
-		else
+		if (!d->name)
 			printf("Name: (nil)\n");
-
-		if (d->age)
-			printf("Age: %f\n", d->age);
-
-		if (d->owner)
-			printf("Owner: %s\n", d->owner);
 		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age:%f\n", d->age);
+
+		if (!d->owner)
 			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 
 	}
 }
