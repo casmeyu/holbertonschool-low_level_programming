9d0d2bd4539f4de63181f8e53605469e437f5fd4
ojo-perezoso <bymartin24@gmail.com>
2021-11-03 08:14:01 -0700

    fix description 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print_name: calls another functions and passes a name
+ * print_name - calls another functions and passes a name
  *
  * @name: name to print
  * @f: function to pass name to
