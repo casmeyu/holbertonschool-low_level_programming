6ea891503f84a92278c8b5d6106327c062652dd9
ojo-perezoso <bymartin24@gmail.com>
2021-11-04 10:27:58 -0700

    fix try 1 1-print_numbers.c

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_numbers(", ", 4, 0, 98, -1024, 402);
+    print_numbers(", ", 3, 1, 2, 3);
     return (0);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (separator)
+		if (separator && (i < (n - 1)))
 			printf("%s", separator);
 	}
-	va_end(ap);
 
-	printf("\n");
+		printf("\n");
+
+	va_end(ap);
 }
