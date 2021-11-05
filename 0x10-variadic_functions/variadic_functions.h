36c8f9eaaafe8ef4cc4cbb8409e5af736f2e0a80
ojo-perezoso <bymartin24@gmail.com>
2021-11-04 19:48:26 -0700

    final fix 3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,20 +3,22 @@
 
 #include <stdarg.h>
 /**
- * struct op_t - option structure
+ * struct op_s - option structure
  *
  *@op: format
  *@f: function associated
  */
-typedef struct {
+typedef struct op_s
+{
 	char *op;
 	void (*f)(va_list ap);
-}op_t;
+} op_t;
 
 void print_char(va_list ap);
 void print_integer(va_list ap);
 void print_float(va_list ap);
 void print_string(va_list ap);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
