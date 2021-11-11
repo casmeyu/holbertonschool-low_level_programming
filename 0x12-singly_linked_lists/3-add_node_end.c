64df4610e5e83b337a35b1111c358ffd7462db17
ojo-perezoso <bymartin24@gmail.com>
2021-11-11 07:18:45 -0800

    Coding style fix and output fix of print_list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 /**
  * add_node - adds a new head node to a linked list
- * head: old head to move
- * str: value of new head->str
- * Returns: the linked list
+ * @head: old head to move
+ * @str: value of new head->str
+ * Return: the linked list
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t new;
+
 	if (*head->next)
 		add_node_end(head->next, str);
 	else
 	{
-		list_t new;
 		new = malloc(sizeof(*new));
 
 		if (!new)
