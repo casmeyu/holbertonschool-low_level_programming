e17ef97b32723b9112dda88c572c0055e5a73a1a
ojo-perezoso <bymartin24@gmail.com>
2021-11-11 05:54:02 -0800

    Added 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a singly linked list
+ * @h: header of the linked list
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	if (!h)
+		return (-1);
+
+	elements++;
+
+	if (h->next)
+		elements += list_len(h->next);
+
+	return (elements);
+}
