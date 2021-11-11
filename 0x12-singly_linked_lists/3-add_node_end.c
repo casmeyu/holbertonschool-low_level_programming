858944062aabbd7e9a8bcd062929fc6fa5b29568
ojo-perezoso <bymartin24@gmail.com>
2021-11-11 11:45:19 -0800

    coding style fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -30,12 +30,12 @@ list_t *add_node_end(list_t **head, const char *str)
 
 		return (new);
 	}
-	
+
 	temp = *head;
 
 	while (temp->next)
 		temp = temp->next;
-	
+
 	temp->next = new;
 
 	return (new);
