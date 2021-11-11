b881bd3d4e8da3527e11d38661bbc17fa216be9c
ojo-perezoso <bymartin24@gmail.com>
2021-11-11 10:21:51 -0800

    Added 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,18 +1,36 @@
 #include "lists.h"
 /**
- * add_node - adds a new head node to a linked list
- * @head: old head to move
- * @str: value of new head->str
- * Return: the linked list
+ * add_node_end - adds a new node at the end of the linked list
+ * @head: head of the linked list
+ * @str: value of new tail->str
+ * Return: the new tail
  */
-list_t *add_node(list_t **head, __attribute__((unused)) const char *str)
+list_t *add_node_end(list_t **head, __attribute__((unused)) const char *str)
 {
+	list_t *current = *head;
+	list_t *new;
 
-	if (head)
+	if (!current)
 	{
-		while ((*head)->next)
-			*head = (*head)->next;
+		if (!str)
+			str = "\0";
+		new = malloc(sizeof(*new));
+
+		if (!new)
+			return (NULL);
+
+		new->str = strdup(str);
+		new->len = strlen(str);
+		new->next = NULL;
+
+		*head = new;
+
+		return (*head);
+	}
+	else
+	{
+		add_node_end(&(current->next), str);
 	}
 
-	return (*head);
+	return (NULL);
 }
