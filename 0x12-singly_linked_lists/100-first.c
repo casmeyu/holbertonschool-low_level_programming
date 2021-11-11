5b6008f8e1d45011198155307dcdb85e957d299d
ojo-perezoso <bymartin24@gmail.com>
2021-11-11 12:39:50 -0800

    Added 100-first.c Thanks geeks for geeks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,8 @@
 /**
  * first - executes before main function
  */
-void first (void)
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
