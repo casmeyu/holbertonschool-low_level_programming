58febdb26f50b719ca5ff64cbacd51957e007a6c
ojo-perezoso <bymartin24@gmail.com>
2021-11-16 14:13:06 -0800

    nothing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,39 +12,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int pos = 0;
 	listint_t *new_node, *tmp_node;
 
-	if (head && (*head))
+	tmp_node = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
 	{
-
-		tmp_node = *head;
-		new_node = malloc(sizeof(listint_t));
-		if (!new_node)
-			return (NULL);
-
-		new_node->n = n;
-
-		if (idx == 0)
-		{
-			new_node->next = (*head);
-			*head = new_node;
-			return (*head);
-		}
-
-		while (pos < idx - 1 && tmp_node)
-		{
-			tmp_node = tmp_node->next;
-			pos++;
-		}
-
-		if (pos >= idx)
-		{
-			free(new_node);
-			return (NULL);
-		}
-
-		new_node->next = tmp_node->next;
-		tmp_node->next = new_node;
-
-		return (new_node);
+		new_node->next = (*head);
+		*head = new_node;
+		return (*head);
 	}
-	return (NULL);
+
+	while (pos < idx - 1 && tmp_node)
+	{
+		tmp_node = tmp_node->next;
+		pos++;
+	}
+
+	if (pos >= idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = tmp_node->next;
+	tmp_node->next = new_node;
+
+	return (new_node);
 }
