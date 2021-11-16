2d19a2a942e3a79d0eda1672fcd4b36da326536c
ojo-perezoso <bymartin24@gmail.com>
2021-11-16 11:02:25 -0800

    final fix 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int pos = 0;
 
-	while (pos != index && head)
+	while (pos < index && head)
 	{
 		pos++;
 		head = head->next;
