bef92cae7714e62554a7004ff834217e68ef518e
ojo-perezoso <bymartin24@gmail.com>
2021-11-16 10:45:15 -0800

    Added 7-get_noint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns a pointer to the desired index
+ * @head: pointer to the singly linked list
+ * @index: node to search for based on index
+ *
+ * Return: pointer to the node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp_node;
+	unsigned int pos = 0;
+
+	if (!head)
+		return (NULL);
+
+	tmp_node = head;
+
+	while (pos <= index && tmp_node)
+	{
+		tmp_node = tmp_node->next;
+		pos++;
+		if (pos == index)
+			return (tmp_node);
+	}
+
+	return (NULL);
+}
