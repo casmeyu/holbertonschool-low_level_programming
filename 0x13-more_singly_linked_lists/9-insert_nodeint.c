0a09e230cfa39def872cb1436a564071807a100b
ojo-perezoso <bymartin24@gmail.com>
2021-11-16 13:58:44 -0800

    fix try 5 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,6 +12,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int pos = 0;
 	listint_t *new_node, *tmp_node;
 
+	if (!head)
+		return (NULL);
+
 	if ((*head))
 	{
 
