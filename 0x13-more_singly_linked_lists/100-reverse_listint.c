f0a5771e7f97cccb4a33c32246c0231195dd37a6
ojo-perezoso <bymartin24@gmail.com>
2021-11-16 18:22:02 -0800

    Added 100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverse a singly linked list
+ * @head: pointer to the linked list
+ *
+ * Return: number of nodes in the list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	unsigned int len = 0;
+	listint_t *prev = NULL, *current = (*head), *next = NULL;
+
+	if (!current)
+		exit(98);
+
+	while (current)
+	{
+		next = current->next;
+
+		current->next = prev;
+
+		prev = current;
+
+		current = next;
+
+		len++;
+	}
+
+	(*head) = prev;
+
+	return (*head);
+}
