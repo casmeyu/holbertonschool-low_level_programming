a9298bc4b9f5a1a7ab5dee1c56cc58a4800b805d
ojo-perezoso <bymartin24@gmail.com>
2021-11-18 07:56:09 -0800

    no fix 1-print_binary.c

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -7,15 +7,26 @@
  */
 int main(void)
 {
+    printf("0\n");
     print_binary(0);
-    printf("\n");
+    printf("\n1\n");
     print_binary(1);
+    printf("\n9\n");
+    print_binary(9);
+    printf("\n10\n");
+    print_binary(10);
+    printf("\n11\n");
+    print_binary(11);
+    printf("\n12\n");
+    print_binary(12);
     printf("\n");
+    /*
     print_binary(98);
     printf("\n");
     print_binary(1024);
     printf("\n");
     print_binary((1 << 10) + 1);
     printf("\n");
+    */
     return (0);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,15 +5,18 @@
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int mask = n;
+
 	if (n == 0)
 		_putchar('0');
-	while (n)
-	{
-		if (n & 1)
-			_putchar('1');
-		else
-			_putchar('0');
+	else
+		while (mask)
+		{
+			if (n & mask)
+				_putchar('1');
+			else
+				_putchar('0');
 
-		n = n >> 1;
-	}
+			mask >>= 1;
+		}
 }
