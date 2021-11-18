dd27ad3c069206562ca82388ccbb40f1c8659846
ojo-perezoso <bymartin24@gmail.com>
2021-11-18 08:54:26 -0800

    final fix 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,8 +5,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 32768;
-	int flag = 0;
+	unsigned int aux_n;
+	int i, flag = 0;
 
 	if (n == 0)
 	{
@@ -14,16 +14,16 @@ void print_binary(unsigned long int n)
 		return;
 	}
 
-	while (mask)
+	for (i = 63; i >= 0; i--)
 	{
-		if (n & mask)
+		aux_n = n >> i;
+
+		if (aux_n & 1)
 		{
 			_putchar('1');
 			flag++;
 		}
 		else if (flag > 0)
 			_putchar('0');
-
-		mask >>= 1;
 	}
 }
