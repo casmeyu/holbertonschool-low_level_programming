bb7bab8e5f218466c906b269f1162c3e35a1c01b
ojo-perezoso <bymartin24@gmail.com>
2021-11-22 08:10:07 -0800

    Added 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: file to read
+ * @letters: number of lettes it should read and print
+ * Return: number of printed letters
+ */
+size_t read_textfile(const char *filename, size_t letters)
+{
+	int f_desc;
+	char *buffer = NULL;
+
+	if (!filename)
+		return (0);
+
+	f_desc = open(filename, O_RDONLY);
+
+	if (f_desc == -1)
+		return (0);
+
+	buffer = malloc(letters * sizeof(char));
+
+	if (!buffer)
+		return (0);
+
+	if (read(f_desc, buffer, letters) < 0)
+		return (0);
+
+	printf("%s", buffer);
+
+
+
+	return (letters);
+}
