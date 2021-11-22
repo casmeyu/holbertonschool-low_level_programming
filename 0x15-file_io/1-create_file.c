e9403b48cd93d6aad56cbd8541bc6b8b77e62834
ojo-perezoso <bymartin24@gmail.com>
2021-11-22 09:47:44 -0800

    fix try O_TRUNC 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	f_desc = open(filename, (O_CREAT | O_RDWR), 0600);
+	f_desc = open(filename, (O_CREAT | O_RDWR | O_TRUNC), 0600);
 
 	if (f_desc == -1)
 		return (-1);
