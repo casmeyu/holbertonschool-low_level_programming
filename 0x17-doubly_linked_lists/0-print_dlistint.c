36219c869e5f3562241d4c13775be2b9028a7d1f
ojo-perezoso <bymartin24@gmail.com>
2022-01-08 08:08:21 -0800

    fix try 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements from a doubly linked list of integers
- * 
- * @head: head of the list
+ * print_dlistint - prints all the elements from a doubly linked list of int
+ *
+ * @h: head of the list
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t length = 0;
-	
+
 	if (!h)
 		return (length);
 
