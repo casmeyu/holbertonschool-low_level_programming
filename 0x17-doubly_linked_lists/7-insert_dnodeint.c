98fcf0d732e3de0baaefbd6baf7824287339478f
ojo-perezoso <bymartin24@gmail.com>
2022-01-09 13:18:37 -0800

    fix try 3 7-insert_dnodeint_at_index.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * insert_dnodeint - insert node in double linked list at given index
+ * insert_dnodeint_at_index - insert node in double linked list at given index
  *
  * @h: pointer to the head node
  * @idx: index of new node
@@ -15,32 +15,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h)
 		return (NULL);
 
-	new_node = malloc(sizeof(*new_node));
-	if (!new_node)
-		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-
-	while (tmp_node && count != idx)
+	while (tmp_node && count < idx)
 	{
+		if ((count + 1) == idx)
+		{
+			new_node = malloc(sizeof(*new_node));
+			if (!new_node)
+				return (NULL);
+
+			new_node->n = n;
+			new_node->next = tmp_node->next;
+			new_node->prev = tmp_node;
+
+			tmp_node->next = new_node;
+			if (tmp_node->next)
+				tmp_node->next->prev = new_node;
+
+			return (new_node);
+		}
 		tmp_node = tmp_node->next;
 		count++;
 	}
 
-	if (count == idx)
-	{
-		 new_node->next = tmp_node->next;
-		 new_node->prev = tmp_node->prev;
-
-		 tmp_node->next->prev = new_node;
-		 tmp_node->next = new_node;
-
-		 return (new_node);
-	}
-
 	return (NULL);
-
-
 }
