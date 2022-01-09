1a022d26758fd33b82c71aff28e9a185962c5b07
ojo-perezoso <bymartin24@gmail.com>
2022-01-09 14:46:29 -0800

    fix try 2 7-insert_dnodeint_at_index.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 			if (tmp_node->next)
 				tmp_node->next->prev = new_node;
-			tmp->next = new_node;
+			tmp_node->next = new_node;
 			return (new_node);
 		}
 		tmp_node = tmp_node->next;
