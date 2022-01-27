22cdbaddb719c17421a505d8e011c718ba32059b
ojo-perezoso <bymartin24@gmail.com>
2022-01-27 06:26:24 -0800

    added 1-djb2.c and 1-hash_table_create but with 3 mallocs?

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - this functions create a hash table based on the hash_table_s struct
+ *
+ * @size: size of the hash table (unsigned long int)
+ * Return: a pointer to the new hash table or NULL if fail
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL;
+
+	new_table = malloc(sizeof(*new_table));
+	if (!new_table)
+		return (NULL);
+	
+	new_table->array = malloc(size * sizeof(hash_node_t *));
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	return (new_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+#0x1A hash tables
+Implementation of hash tables with the c language
+A hash table is an array of linked list
