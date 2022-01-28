fdbc50130c0e078a1d1c3b79fdd38ec47cb4d244
ojo-perezoso <bymartin24@gmail.com>
2022-01-28 05:46:19 -0800

    code style fix for hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -13,9 +13,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -28,8 +28,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 /* Protorypes */
@@ -38,7 +38,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 /* Aux Functions */
-hash_node_t *add_hash_node(hash_node_t **head, const char *value, const char *key);
+hash_node_t *add_hash_node(hash_node_t **h, const char *val, const char *key);
 
 /**
  * add_node - adds a new head node to a linked list
