00bdc4f845369f86e5daf2cf040d8daadc42f4a2
ojo-perezoso <bymartin24@gmail.com>
2022-03-17 12:02:27 -0700

    fix: fixed islands on the borders

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,21 +5,24 @@
 def island_perimeter(grid):
     """Returns the perimeter of the islands"""
     perimeter = 0
+    
+    if not grid:
+        return (0)
 
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                block_p = 4;
+                block_p = 4
                 if x > 0:
                     if grid[x - 1][y] == 1:
                         block_p -= 1
-                if x < len(grid):
+                if x < len(grid) - 1:
                     if grid[x + 1][y] == 1:
                         block_p -= 1
                 if y > 0:
                     if grid[x][y - 1] == 1:
                         block_p -= 1
-                if y < len(grid[x]):
+                if y < len(grid[x]) - 1:
                     if grid[x][y + 1] == 1:
                         block_p -= 1
 
